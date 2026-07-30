6d81fc220c1906cf0c38b6435eedd90b302f2be1
LanRhyme <xiao_ren233@foxmail.com>
2026-07-30 22:20:11 +0800

    auto: 2026-07-30-22:20:11

--- dot_config/noctalia/morandi-gen.py
+++ dot_config/noctalia/morandi-gen.py
@@ -371,7 +371,7 @@ def write_fcitx5(palette):
     border = hsl_to_hex(bh, bs, min(bl + 5, 100))
 
     panel_svg = f'''<svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
-<rect width="40" height="40" rx="8" fill="{bg}" fill-opacity="0.95"/>
+<rect width="40" height="40" rx="8" fill="{bg}" fill-opacity="0.85"/>
 <rect x="0.5" y="0.5" width="39" height="39" rx="8" stroke="{border}" stroke-opacity="1.0"/>
 </svg>'''
 
